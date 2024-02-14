9dc366f2f8f503c5d68ccf4e1a37243686886fea
opilane <opilane@HP-CZC2349HT3>
2024-02-14 11:40:12 +0200

    Add project files.

---
+++ PythonApplication4.py
@@ -0,0 +1,95 @@
+from string import *
+#1
+#vokaali=["a","e","u","o","i","ü","ö","õ","ä"]
+#konsonanti="qwrtpsdfghjklzxcvbnm"
+#markid=punctuation
+#v=k=m=t=0
+#tekst=input("Sisesta sõna vöi lause: ").lower()
+#tekst_list=list(tekst)
+#    if sümbol in vokaali:
+#        v+=1
+#    elif sümbol in konsonanti:
+#        k+=1
+#    elif sümbol in markid:
+#        m+=1
+#    elif sümbol==" ":
+#        t+=1
+#print("Vokaali:",v,"\nKonsonanti:",k)
+#print("Kirjuvahemärgid:",m)
+#print("Tühikud:",t)
+
+
+#2
+
+#nimed=[]
+#for i in range(5):
+#    nimi=(input("Sisesta nimi: "))
+#    nimed.append(nimi)
+#print("Loetelu oli: ",nimed)
+#nimed.sort()
+#print("Loetelu sorteeritud: ",nimed)
+#for n in range(len(nimed)):
+#    print(n+1,".",nimed[n],sep="")
+#print("VImasena oli lisatud: ",nimi)
+
+#uued_nimi=[]
+#for nimi in nimed:
+#    if nimi not in uued_nimi:
+#        uued_nimi.append(nimi)
+#print(uued_nimi)
+#uued_nimi=list(set(nimed))
+#vanused=[]
+#for i in range(5):
+#    vanus=int(input("Sisesta vanus: "))
+#    vanused.append(vanus)
+#maksimum=max(vanused)
+#minimum=min(vanused)
+#summa=sum(vanused)
+#keskmine=summa/len(vanused)
+#for i in range(5):
+#    print(nimed[i], "on ", vanused[i],"aastat vana")
+
+
+##3
+#from random import *
+#arvud=[]
+#N=int(input("Mitu rida joonistame? "))
+#S=input("Sissesta sümbol: ")
+#for p in range(N):
+#    arvud.append(randint(1,100))
+#for p in range(N):
+#    print(arvud[p]*S)
+
+
+##4
+
+#postiindeks=["Tallinn","Narva, Narva-Jõesuu","Kohtla-Järve"," Ida-Virumaa, Lääne-Virumaa, Jõgevamaa","Tartu linn","Tartumaa, Põlvamaa, Võrumaa, Valgamaa","Viljandimaa, Järvamaa, Harjumaa, Raplamaa","Pärnumaa"," Läänemaa, Hiiumaa, Saaremaa"]
+#while True:
+#    while True:
+#        try:
+#            indeks=int(input("Sisesta viienumbrilline indeks: "))
+#            indeks_elem_arv=len(str(indeks))
+#            if indeks_elem_arv==5:
+#                print("Snumbriline indeks ")
+#                break
+#            else:
+#                print("On vaja Snumbriline indeks ")
+
+
+#5
+#while True
+#    try:
+#        elem=[input("Sisestage elemente ")]
+#        õigus=input("tahad rohkem valjuta 1, ei 0")
+#        if õigus=1:
+        
+#    except:
+#        pass
+
+#6
+
+
+
+
+
+
